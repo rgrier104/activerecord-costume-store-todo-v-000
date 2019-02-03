d9200b127c1debfe878a52717d204758cc03d28a
Rachel Grier <rgrier104@gmail.com>
2019-02-03 01:21:52 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,7 +15,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
       t.string :name
       t.float :price
       t.integer :size
-      t.string :imageurl
+      t.string :image_url
       t.timestamps
     end
   end
